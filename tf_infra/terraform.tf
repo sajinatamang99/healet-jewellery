75cba6d1aa9a1414c5461ed97342e03c8095243a
sajinatamang99 <167822180+sajinatamang99@users.noreply.github.com>
2025-05-16 23:50:37 +0100

    add trivy scan error fix

--- tf_infra/terraform.tf
+++ tf_infra/terraform.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket       = "tf-s3-backend-bucket453"
-    key          = "tf_infra/terraform.tfstate"
-    region       = "eu-west-2"
+    bucket         = "tf-s3-backend-bucket453"
+    key            = "tf_infra/terraform.tfstate"
+    region         = "eu-west-2"
     dynamodb_table = "terraform-locks"
-    encrypt      = true
+    encrypt        = true
   }
   required_version = ">= 1.0.0"
   required_providers {
