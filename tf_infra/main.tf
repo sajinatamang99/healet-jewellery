7051340e550dd350a45eaa3171f5753cdf29eb3b
sajinatamang99 <167822180+sajinatamang99@users.noreply.github.com>
2025-05-20 19:32:18 +0100

    add sonarqube

--- tf_infra/main.tf
+++ tf_infra/main.tf
@@ -45,8 +45,8 @@ resource "aws_instance" "webserver" {
     Environment = "demo"          # Optional additional tags
   }
   metadata_options {
-  http_tokens   = "required"
-  http_endpoint = "enabled"
+    http_tokens   = "required"
+    http_endpoint = "enabled"
   }
   root_block_device {
     encrypted = true
