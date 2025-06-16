1ef3257bcc011a44c6137d1cca840c675deff9c8
sajinatamang99 <167822180+sajinatamang99@users.noreply.github.com>
2025-06-16 23:27:11 +0100

    implement prometheus & grafana and few ec2 server changes

--- tf_infra/modules/monitoring/install-monitoring.sh
+++ tf_infra/modules/monitoring/install-monitoring.sh
@@ -3,12 +3,10 @@
 sudo apt-get update -y
 sudo apt-get install -y wget curl gnupg2 software-properties-common
 
-
 # --- Install Prometheus ---
 sudo useradd --no-create-home --shell /bin/false prometheus
 sudo mkdir /etc/prometheus
 sudo mkdir /var/lib/prometheus
-
 
 cd /tmp
 PROM_VERSION="2.45.0"
@@ -20,13 +18,11 @@ sudo cp promtool /usr/local/bin/
 sudo cp -r consoles/ console_libraries/ /etc/prometheus/
 sudo cp prometheus.yml /etc/prometheus/
 
-
 # --- Prometheus service ---
 cat <<EOF | sudo tee /etc/systemd/system/prometheus.service
 [Unit]
 Description=Prometheus
 After=network.target
-
 
 [Service]
 User=prometheus
@@ -34,17 +30,41 @@ ExecStart=/usr/local/bin/prometheus \
   --config.file=/etc/prometheus/prometheus.yml \
   --storage.tsdb.path=/var/lib/prometheus/
 
-
 [Install]
 WantedBy=multi-user.target
 EOF
-
 
 sudo systemctl daemon-reexec
 sudo systemctl daemon-reload
 sudo systemctl enable prometheus
 sudo systemctl start prometheus
 
+# --- Install Node Exporter ---
+cd /tmp
+NODE_EXPORTER_VERSION="1.8.0"
+wget https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
+tar xvf node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
+cp node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64/node_exporter /usr/local/bin/
+useradd --no-create-home --shell /bin/false node_exporter
+
+# Node Exporter service
+cat <<EOF > /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Node Exporter
+After=network.target
+
+[Service]
+User=node_exporter
+ExecStart=/usr/local/bin/node_exporter
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable node_exporter
+systemctl start node_exporter
 
 # --- Install Grafana ---
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
