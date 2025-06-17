435d8187d3b113cd3c9a2e01a75b4b0608faf17a
sajinatamang99 <167822180+sajinatamang99@users.noreply.github.com>
2025-06-17 23:41:29 +0100

    implement prometheus & grafana and few ec2 server changes

--- tf_infra/modules/monitoring/main.tf
+++ tf_infra/modules/monitoring/main.tf
@@ -5,7 +5,6 @@ resource "aws_instance" "monitoring" {
   instance_type               = var.instance_type
   vpc_security_group_ids      = var.monitoring_sg_ids
   subnet_id                   = var.monitoring_subnet_ids
-  associate_public_ip_address = true
   user_data                   = file("${path.module}/install-monitoring.sh")
   tags = {
     Name = "Prometheus-Monitoring-Server"
