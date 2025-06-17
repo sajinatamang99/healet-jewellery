944df0d8809e045f4bda16617350cbe07e0e6395
sajinatamang99 <167822180+sajinatamang99@users.noreply.github.com>
2025-06-17 23:57:54 +0100

    Feat4: "implement prometheus & grafana and few ec2 server changes" (#9)
    * Implement prometheus & grafana, also few changes in ec2 web server
    
    * Implement prometheus & grafana, also few changes in ec2 web server
    
    * implement prometheus & grafana and few ec2 server changes
    
    * implement prometheus & grafana and few ec2 server changes
    
    * implement prometheus & grafana and few ec2 server changes
    
    * implement prometheus & grafana and few ec2 server changes
    
    * implement prometheus & grafana and few ec2 server changes
    
    * implement prometheus & grafana and few ec2 server changes
    
    * implement prometheus & grafana and few ec2 server changes
    
    * implement prometheus & grafana and few ec2 server changes
    
    * implement prometheus & grafana and few ec2 server changes
    
    * implement prometheus & grafana and few ec2 server changes
    
    * implement prometheus & grafana and few ec2 server changes
    
    * implement prometheus & grafana and few ec2 server changes
    
    * implement prometheus & grafana and few ec2 server changes
    
    * implement prometheus & grafana and few ec2 server changes
    
    * implement prometheus & grafana and few ec2 server changes
    
    * implement prometheus & grafana and few ec2 server changes
    
    * implement prometheus & grafana and few ec2 server changes

---
+++ tf_infra/modules/monitoring/main.tf
@@ -0,0 +1,12 @@
+# Create an EC2 instance for prometheus monitoring server with the below specifications
+resource "aws_instance" "monitoring" {
+  key_name                    = var.monitoring_keypair
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  vpc_security_group_ids      = var.monitoring_sg_ids
+  subnet_id                   = var.monitoring_subnet_ids
+  user_data                   = file("${path.module}/install-monitoring.sh")
+  tags = {
+    Name = "Prometheus-Monitoring-Server"
+  }
+}
